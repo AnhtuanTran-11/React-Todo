81ec0c25b3910079cf58750b03858aff219e8059
Anhtuan Tran <anhtuantran.github@gmail.com>
2021-02-08 23:57:53 -0600

    Saved changes

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,12 @@
-import React from 'react';
+// import React from 'react';
 
-const Todo = props => {
-    return (
-        <div onClick={() => props.toggleItem(props.item.id)} className={`item${props.item.completed ? ' completed' : ""}`}>
-          <p>{props.item.task}</p>
-        </div>
+// const Todo = props => {
+//     return (
+//         <div onClick={() => props.toggleItem(props.item.id)} className={`item${props.item.completed ? ' completed' : ""}`}>
+//           <p>{props.item.task}</p>
+//         </div>
     
-      );
-}
+//       );
+// }
 
-export default Todo;
+// export default Todo;
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class TodoForm extends React.Component {
+// class TodoForm extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            newTask: "",
-            tasks: []
-        }
-    }
-    handleChanges = event => {
-        this.setState({
-            newTodo: event.target.value,
-        })
-    }
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             newTask: "",
+//             tasks: []
+//         }
+//     }
+//     handleChanges = event => {
+//         this.setState({
+//             newTodo: event.target.value,
+//         })
+//     }
 
-    render() {
-        const { addTask, updateTasks } = this.props;
-    return (
-            <form>
-            <input
-              type="text"
-              name="tasks"
-              placeholder="Add a todo here"
-              value={this.state.newTask}
-              onChange={event => this.props.updateTasks("newTask", event.target.value)}
-            />
-            <button onClick={() => addTask()}>Add todo</button>
-            <button>Clear completed</button>
-            </form>
-    )
-    }}
+//     render() {
+//         const { addTask, updateTasks } = this.props;
+//     return (
+//             <form>
+//             <input
+//               type="text"
+//               name="tasks"
+//               placeholder="Add a todo here"
+//               value={this.state.newTask}
+//               onChange={event => this.props.updateTasks("newTask", event.target.value)}
+//             />
+//             <button onClick={() => addTask()}>Add todo</button>
+//             <button>Clear completed</button>
+//             </form>
+//     )
+//     }}
 
-    export default TodoForm;
+//     export default TodoForm;
